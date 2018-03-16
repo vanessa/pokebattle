6a8182840769b853ea36c2069b326816c09d2968
vanessa <vanessa@vinta.com.br>
2018-03-16 01:58:40 -0300

    Add basic request of Pokemon stats from CreateBattleForm

--- battles/forms.py
+++ battles/forms.py
@@ -1,17 +1,17 @@
 from django import forms
 from django.core.exceptions import ValidationError
-
 from .models import Battle
 
 
 class CreateBattleForm(forms.ModelForm):
     class Meta:
         model = Battle
-        fields = ['opponent', 'creator']
+        fields = ['opponent']
 
     first_pokemon = forms.CharField(required=True, label='Insert first Pokemon ID')
     second_pokemon = forms.CharField(required=True, label='Insert second Pokemon ID')
     third_pokemon = forms.CharField(required=True, label='Insert third Pokemon ID')
 
-    def clean(self):
+    def clean(self, **kwargs):
         cd = super().clean() # cd = short for cleaned data
+        return cd
--- battles/views.py
+++ battles/views.py
@@ -1,6 +1,10 @@
+from django.views import generic
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
-from django.views import generic
+
+import pokebase as pb
+
+from pokemons.models import Pokemon
 
 from .forms import CreateBattleForm
 
@@ -18,4 +22,18 @@ class CreateBattleView(generic.CreateView):
     def form_valid(self, form):
         data = form.instance
         data.creator = self.request.user
+        pokemons = []
+        pokemons.extend([
+            form.cleaned_data['first_pokemon'],
+            form.cleaned_data['second_pokemon'],
+            form.cleaned_data['third_pokemon']
+        ])
+
+        for pokemon_id in pokemons:
+            try:
+                query = Pokemon.objects.get(id=pokemon_id)
+            except Pokemon.DoesNotExist:
+                pokemon_data = pb.pokemon(pokemon_id)
+                stats = [(stats.stat.name, stats.base_stat) for stats in pokemon_data.stats]
+            print('continue | ' + pokemon_id)
         return super().form_valid(form)
