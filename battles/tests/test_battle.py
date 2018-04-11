71d104df2d5b301b049b44f574f2ae7d10c49913
vanessa <vanessa@vinta.com.br>
2018-04-11 16:08:47 -0300

    More tests

--- battles/tests/test_battle.py
+++ battles/tests/test_battle.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 
 from model_mommy import mommy
 
-from battles.helpers import compare_two_pokemons
+from battles.helpers import can_run_battle, compare_two_pokemons
 from common.utils.tests import TestCaseUtils
 
 
@@ -15,6 +15,22 @@ class TestBattle(TestCaseUtils, TestCase):
             'pokemons.Pokemon', name='Creatorpokemon', attack=60, defense=40, hp=30)
         self.opponent_pokemon = mommy.make(
             'pokemons.Pokemon', name='Opponentpokemon', attack=30, defense=50, hp=10)
+
+    def test_can_run_battle_with_a_team(self):
+        mommy.make(
+            'battles.BattleTeam', battle_related=self.battle,
+            pokemons=mommy.make('pokemons.Pokemon', _quantity=3),
+            trainer=self.battle.creator
+        )
+        mommy.make(
+            'battles.BattleTeam', battle_related=self.battle,
+            pokemons=mommy.make('pokemons.Pokemon', _quantity=3),
+            trainer=self.battle.opponent
+        )
+        self.assertTrue(can_run_battle(self.battle.id))
+
+    def test_cannot_run_battle_without_a_team(self):
+        self.assertFalse(can_run_battle(self.battle.id))
 
     def test_two_pokemon_comparison(self):
         winner = compare_two_pokemons(
