3e4517052c6f3894eb2a6cf759659b0de28ca44a
vanessa <vanessa@vinta.com.br>
2018-04-24 15:22:11 -0300

    Add views tests

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.urls import reverse_lazy
+from django.urls import resolve, reverse_lazy
 
 from model_mommy import mommy  # noqa
 
@@ -14,6 +14,11 @@ class TestUserSignupView(TestCaseUtils, TestCase):
         super().setUp()
         self.view_url = reverse_lazy('auth:signup')
         self.view_class = UserSignupView()
+        self.success_url = reverse_lazy('battles:list')
+
+    def test_user_signup_url_is_connected_to_view(self):
+        self.assertEqual(resolve(self.view_url).func.__name__,
+                         UserSignupView.as_view().__name__)
 
     def test_get_correct_form(self):
         form_class = self.view_class.get_form_class()
@@ -23,6 +28,10 @@ class TestUserSignupView(TestCaseUtils, TestCase):
         response = self.auth_client.get(self.view_url)
         self.assertEqual(response.status_code, 302)
 
+    def test_access_to_signup_view_returns_200(self):
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, 200)
+
     def test_signup_redirects_to_battle_list(self):
         user_params = {
             'email': 'test@vinta.com.br',
@@ -30,9 +39,8 @@ class TestUserSignupView(TestCaseUtils, TestCase):
             'password2': 'testpassword123'
         }
         response = self.client.post(self.view_url, user_params)
-        success_url = reverse_lazy('battles:list')
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, success_url)
+        self.assertRedirects(response, self.success_url)
 
     def test_signup_redirection_has_welcome_message(self):
         user_params = {
@@ -43,3 +51,13 @@ class TestUserSignupView(TestCaseUtils, TestCase):
         response = self.client.post(self.view_url, user_params)
         message_count = len(response.cookies['messages'])
         self.assertTrue(message_count > 0)
+
+    def test_user_signup(self):
+        user_params = {
+            'email': 'test@vinta.com.br',
+            'password1': 'testpassword123',
+            'password2': 'testpassword123'
+        }
+        response = self.client.post(self.view_url, user_params)
+        self.assertTrue(response.status_code, 302)
+        self.assertRedirects(response, self.success_url)
