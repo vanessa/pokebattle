b675483bb92f79d43163d2aa51d89d40a90854e0
vanessa <vanessa@vinta.com.br>
2018-05-22 17:23:46 -0300

    Fix test which validates the invite

--- users/tests/test_pipeline.py
+++ users/tests/test_pipeline.py
@@ -4,7 +4,7 @@ from social_django.utils import load_backend, load_strategy
 
 from battles.models import Battle
 from common.utils.tests import TestCaseUtils
-from users.auth_pipeline import _handle_invite
+from users.auth_pipeline import _handle_invite, validate_invite_key
 
 
 class TestInviteSignup(TestCaseUtils):
@@ -14,12 +14,12 @@ class TestInviteSignup(TestCaseUtils):
         self.strategy = load_strategy()
         self.backend = load_backend(strategy=self.strategy, name='google-oauth2', redirect_uri='/')
 
-    @mock.patch('users.auth_pipeline.validate_invite_key')
-    def test_user_signed_up_with_invite(self, validate_key):
-        mommy.make('battles.Invite', key=123)
-        validate_key.return_value = setattr(self.user, 'has_invite', True)
-        validate_key(user=self.user, strategy=self.strategy,
-                     backend=self.backend, details={})
+    @mock.patch('social_django.utils.load_strategy')
+    def test_user_signed_up_with_invite(self, strategy):
+        mommy.make('battles.Invite', key=123, invitee=self.user.email)
+        strategy.session_get.return_value = '123'
+        validate_invite_key(user=self.user, strategy=strategy,
+                            backend=self.backend, details={})
         self.assertTrue(getattr(self.user, 'has_invite'))
 
     def test_user_signup_with_invite_creates_battle(self):
