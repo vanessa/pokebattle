3cb3a5005cb86dbc57340f083121c1522d4bccd3
vanessa <vanessa@vinta.com.br>
2018-05-28 17:13:34 -0300

    Remove time.sleep, task now working

--- battles/tasks/battle.py
+++ battles/tasks/battle.py
@@ -1,5 +1,3 @@
-import time
-
 from battles.helpers.battle import run_battle
 from battles.models import Battle
 from pokebattle import celery_app
@@ -7,6 +5,5 @@ from pokebattle import celery_app
 
 @celery_app.task
 def run_battle_task(battle_id):
-    time.sleep(5)
     battle = Battle.objects.get(id=battle_id)
     run_battle(battle)
