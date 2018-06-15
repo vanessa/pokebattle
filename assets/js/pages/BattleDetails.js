306453eebeac21fce10bbc38f7f38871739ac5db
vanessa <vanessa@vinta.com.br>
2018-06-15 17:37:30 -0300

    Add placeholder components

--- assets/js/pages/BattleDetails.js
+++ assets/js/pages/BattleDetails.js
@@ -35,6 +35,8 @@ const PokemonName = styled.div`
     font-size: 1.3em;
     font-weight: bold;
     text-align: center;
+    display: block;
+    position: relative;
 `;
 
 const PokemonPic = styled.img`
@@ -53,11 +55,38 @@ const WinnerContainer = styled.div`
     display: flex;
 `;
 
-function TeamDetails(props) {
+function PokemonLoading(props) {
+  return (
+    <div className="placeholder-card">
+      {props.content}
+    </div>
+  );
+}
+
+function PokemonInfoPlaceholder() {
+  const repeater = [1, 2, 3];
+  return (
+    <div>
+      {repeater.map(i => (
+        <div className="pokemon-card inactive" key={i}>
+          <div className="pokemon-pic placeholder-comp" />
+          <div className="pokemon-name placeholder-comp" />
+          <div className="attributes">
+            <span className="placeholder-comp" />
+            <span className="placeholder-comp" />
+            <span className="placeholder-comp" />
+          </div>
+        </div>))
+      }
+    </div>
+  );
+}
+
+function PokemonInfo(props) {
   return (
     <div className="team-column">
       {props.team.map(pokemon => (
-        <PokemonCard key={pokemon.id}>
+        <PokemonCard key={pokemon.name}>
           <PokemonPic src={pokemon.sprite} />
           <PokemonName>{pokemon.name}</PokemonName>
           <PokemonStats>
@@ -71,40 +100,15 @@ function TeamDetails(props) {
   );
 }
 
-function PokemonLoading() {
-  return (
-    <div className="placeholder-card" />
-  );
+function TeamDetails(props) {
+  const battleFinished = props.battleStatus === 'F';
+  const { team } = props;
+
+  if (!battleFinished) {
+    return <PokemonInfoPlaceholder />;
+  }
+  return <PokemonInfo team={team} />;
 }
-
-TeamDetails.propTypes = {
-  team: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    creator: PropTypes.shape({
-      username: PropTypes.string.isRequired,
-      pokemons: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.number,
-        name: PropTypes.string,
-        sprite: PropTypes.string,
-        attack: PropTypes.number,
-        defense: PropTypes.number,
-        hp: PropTypes.number,
-      })),
-    }),
-    opponent: PropTypes.shape({
-      username: PropTypes.string.isRequired,
-      pokemons: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.number,
-        name: PropTypes.string,
-        sprite: PropTypes.string,
-        attack: PropTypes.number,
-        defense: PropTypes.number,
-        hp: PropTypes.number,
-      })),
-    }),
-  })).isRequired,
-};
-
 
 class BattleDetails extends React.Component {
   constructor(props) {
@@ -140,6 +144,7 @@ class BattleDetails extends React.Component {
     if (battle) {
       const creatorTeam = battle.creator.pokemons;
       const opponentTeam = battle.opponent.pokemons;
+      const battleId = this.props.match.params.pk;
 
       return (
         <Container>
@@ -148,7 +153,7 @@ class BattleDetails extends React.Component {
           {!battle.winner &&
             <a
               style={{ textAlign: 'center', display: 'block' }}
-              href={window.Urls['battles:team'](this.props.match.params.pk)}
+              href={window.Urls['battles:team'](battleId)}
             >
               Choose team
             </a>
@@ -162,14 +167,20 @@ class BattleDetails extends React.Component {
             className={`battle-row ${this.getWinnerPosition()}`}
           >
             {!creatorTeam
-              ? <PokemonLoading />
+              ? <PokemonLoading
+                content={`Waiting for ${battle.creator.username} to choose their team`}
+              />
               : <TeamDetails
+                battleStatus={battle.status}
                 team={creatorTeam}
               />
             }
             {!opponentTeam
-              ? <PokemonLoading />
+              ? <PokemonLoading
+                content={`Waiting for ${battle.opponent.username} to choose their team`}
+              />
               : <TeamDetails
+                battleStatus={battle.status}
                 team={opponentTeam}
               />
             }
@@ -187,6 +198,67 @@ BattleDetails.propTypes = {
       pk: PropTypes.string,
     }),
   }).isRequired,
+};
+
+PokemonLoading.propTypes = {
+  content: PropTypes.string.isRequired,
+};
+
+TeamDetails.propTypes = {
+  team: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    creator: PropTypes.shape({
+      username: PropTypes.string.isRequired,
+      pokemons: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        sprite: PropTypes.string,
+        attack: PropTypes.number,
+        defense: PropTypes.number,
+        hp: PropTypes.number,
+      })),
+    }),
+    opponent: PropTypes.shape({
+      username: PropTypes.string.isRequired,
+      pokemons: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        sprite: PropTypes.string,
+        attack: PropTypes.number,
+        defense: PropTypes.number,
+        hp: PropTypes.number,
+      })),
+    }),
+  })).isRequired,
+  battleStatus: PropTypes.string.isRequired,
+};
+
+PokemonInfo.propTypes = {
+  team: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    creator: PropTypes.shape({
+      username: PropTypes.string.isRequired,
+      pokemons: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        sprite: PropTypes.string,
+        attack: PropTypes.number,
+        defense: PropTypes.number,
+        hp: PropTypes.number,
+      })),
+    }),
+    opponent: PropTypes.shape({
+      username: PropTypes.string.isRequired,
+      pokemons: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        sprite: PropTypes.string,
+        attack: PropTypes.number,
+        defense: PropTypes.number,
+        hp: PropTypes.number,
+      })),
+    }),
+  })).isRequired,
 };
 
 export default BattleDetails;
