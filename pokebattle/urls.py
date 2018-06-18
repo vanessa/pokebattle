b7c3c401ed8bdb5f9e9d4a665ac863c36beb6784
vanessa <vanessa@vinta.com.br>
2018-06-18 12:55:49 -0300

    Add test to check if Token was created and add obtain_auth_token view

--- pokebattle/urls.py
+++ pokebattle/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 import django_js_reverse.views
+from rest_framework.authtoken import views as drf_views
 
 from battles.views import PokemonListAPIView
 
@@ -22,6 +23,7 @@ urlpatterns = [
     # API
     url(r'^api/pokemon/$', PokemonListAPIView.as_view(), name='api-pokemon-list'),
     url(r'api/', include('battles.endpoints_urls', namespace='api-battles')),
+    url('api-token-auth/', drf_views.obtain_auth_token, name='obtain-auth-token')
 ]
 
 if settings.DEBUG:
