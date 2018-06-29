8c283fb09472cbb5e7ceaa88f516148329450b5f
vanessa <vanessa@vinta.com.br>
2018-06-29 16:58:51 -0300

    Create the basic layout of the battle list

--- assets/js/pages/BattleList.js
+++ assets/js/pages/BattleList.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const BattleList = () => <h2>Your battles</h2>;
+const BattlesColumn = ({ title }) => (
+  <div className="battles-grid-column">
+    <h3>{title}</h3>
+    <div className="battle-list">
+      {
+        [1, 2, 3, 4].map(battle => <div key={battle}>{battle}</div>)
+      }
+    </div>
+  </div>
+);
+
+const BattleList = () => (
+  <div className="battle-list-container">
+    <h2>My battles</h2>
+    <div className="battles-grid">
+      <BattlesColumn
+        title="Battles you created"
+      />
+      <BattlesColumn
+        title="Battles you were invited"
+      />
+    </div>
+  </div>
+);
+
+BattlesColumn.propTypes = {
+  title: PropTypes.string,
+};
+
+BattlesColumn.defaultProps = {
+  title: '',
+};
 
 export default BattleList;
