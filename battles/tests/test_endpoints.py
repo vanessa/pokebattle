ab18fe9e2870f8b4f8a2b175314da7b515cac2b4
vanessa <vanessa@vinta.com.br>
2018-07-02 16:43:56 -0300

    Add test for endpoints

--- battles/tests/test_endpoints.py
+++ battles/tests/test_endpoints.py
@@ -22,3 +22,29 @@ class TestBattleDetailsEndpoint(TestCaseUtils):
         view_url = reverse('api-battles:battle-details', args=[battle.pk])
         response = self.client.get(view_url)
         self.assertResponse403(response)
+
+
+class TestBattleListEndpoint(TestCaseUtils):
+    def test_fetch_correct_list(self):
+        mommy.make('battles.Battle', creator=self.user, _quantity=3)
+        view_url = reverse('api-battles:battle-list')
+        response = self.auth_client.get(view_url)
+        data = response.data
+        self.assertEqual(len(data), 3)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_fetch_battles_that_user_participates(self):
+        # Battles that user is creator or opponent
+        mommy.make('battles.Battle', creator=self.user, _quantity=3)
+        # Random battles which user isn't part of
+        mommy.make('battles.Battle', _quantity=2)
+        view_url = reverse('api-battles:battle-list')
+        response = self.auth_client.get(view_url)
+        data = response.data
+        self.assertEqual(len(data), 3)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_fetch_battles_when_logged_out(self):
+        view_url = reverse('api-battles:battle-list')
+        response = self.client.get(view_url)
+        self.assertResponse403(response)
