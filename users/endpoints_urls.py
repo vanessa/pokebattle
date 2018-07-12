5cc32368d7e29a1a5030ec304fbd7df9b233db20
vanessa <vanessa@vinta.com.br>
2018-07-12 17:02:28 -0300

    Add user list endpoint and tests accordingly

--- users/endpoints_urls.py
+++ users/endpoints_urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from users.endpoints import UserDetailsEndpoint
+from users.endpoints import UserDetailsEndpoint, UsersEndpoint
 
 
 urlpatterns = [
-    url(r'^user-details/$', UserDetailsEndpoint.as_view(), name='user-details')
+    url(r'^user-details/$', UserDetailsEndpoint.as_view(), name='user-details'),
+    url(r'^users/$', UsersEndpoint.as_view(), name='users'),
 ]
